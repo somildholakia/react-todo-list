a732a927806074b4adf133ab8976c75a7c050e47
somildholakia <somildholakia0852@gmail.com>
2026-03-02 18:12:19 +0530

    update

--- react-todo-List/src/TodoList.jsx
+++ react-todo-List/src/TodoList.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 export default function TodoList(){
+
+    let [todo,setTodo] = useState(["Somil","ritik"]);
+    let [newTodo, setNewTodo] = useState("");
+
+
+    let addNewTask = () => {
+         
+    }
+
     return(
         <div>
+            <input placeholder="Add a Task" value={newTodo}></input> &ensp;
+            <button>Add Task</button>
+            &ensp; &ensp; &ensp;
+
             <h2>Todo List</h2>
+            <ul>
+                    {
+                        todo.map((todo) => {
+                       return <li>{todo}</li>
+})
+                    }
+            </ul>
         </div>
     );
 }
