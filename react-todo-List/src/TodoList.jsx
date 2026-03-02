2f91a8c416036b1471feded7245cc2323e3a61e7
somildholakia <somildholakia0852@gmail.com>
2026-03-02 20:23:25 +0530

    updated the ui

--- react-todo-List/src/TodoList.jsx
+++ react-todo-List/src/TodoList.jsx
@@ -1,26 +1,33 @@
 import { useState } from "react";
+import "./TodoList.css";
 
 export default function TodoList(){
 
-    let [todo,setTodo] = useState(["Somil","ritik"]);
+    let [todo,setTodo] = useState(["learn react","learn redux"]);
     let [newTodo, setNewTodo] = useState("");
 
 
     let addNewTask = () => {
-         
+         setTodo([...todo,newTodo])
+         setNewTodo("");
+    }
+
+    let updateTodoValue = (event) => {
+        setNewTodo(event.target.value);
     }
 
     return(
         <div>
-            <input placeholder="Add a Task" value={newTodo}></input> &ensp;
-            <button>Add Task</button>
+            <h1>Enter the Task</h1>
+            <input className="inputField" placeholder="Add a Task" value={newTodo} onChange={updateTodoValue}></input> &ensp;<br/>
+            <button className="inputbtn" onClick={addNewTask}>Add Task</button>
             &ensp; &ensp; &ensp;
 
-            <h2>Todo List</h2>
+            <h2 className="todoLists">Todo List</h2>
             <ul>
                     {
                         todo.map((todo) => {
-                       return <li>{todo}</li>
+                       return <li className="liLists">{todo}</li>
 })
                     }
             </ul>
