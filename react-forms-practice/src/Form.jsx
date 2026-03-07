fdf29082a88894059b8381bead91fa9f6d5e28af
somildholakia <somildholakia0852@gmail.com>
2026-03-07 18:02:58 +0530

    Added a new mini for forms_react

---
+++ react-forms-practice/src/Form.jsx
@@ -0,0 +1,10 @@
+export default function Form(){
+    return (
+        <div>
+            <h2>Forms</h2>
+            <form>
+                <input placeholder="Enter full name"></input>
+            </form>
+        </div>
+    );
+}
